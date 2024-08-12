697d1bd41cdd74328027648158d3a4bf0df232cd
someone-is <shouryashekhar.31december@gmail.com>
2024-08-12 22:39:51 +0530

    Bug Fix - Try 2 - Token Handeling

--- app/Api/Login/route.js
+++ app/Api/Login/route.js
@@ -7,32 +7,34 @@ import jwt from "jsonwebtoken";
 export async function POST(request) {
     await ConnectDatabase();
     const { userId, password } = await request.json();
-    
+
     try {
-        const Userindb = await User.findOne({userId: userId})
+        const Userindb = await User.findOne({ userId: userId })
         if (!Userindb) {
             throw new Error("Either User-id or Password is Incorrect")
             // return NextResponse.json({result:"Either User-id or Password is Incorrect", success: false})
         }
-        else{
-            if (bcrypt.compareSync(password,Userindb.password)) {
-                const token = jwt.sign({_id:Userindb.userId, name:Userindb.name, as:Userindb.as},process.env.JWT_CODE)
-                const response = NextResponse.json({result: `${Userindb.name} has Logged in`, success: true})
+        else {
+            if (bcrypt.compareSync(password, Userindb.password)) {
+                const token = jwt.sign({ _id: Userindb.userId, name: Userindb.name, as: Userindb.as }, process.env.JWT_CODE)
+                const response = NextResponse.json({ result: `${Userindb.name} has Logged in`, success: true })
                 // response.cookies.set("Login Token", token,{expiresIn: 10})
                 response.cookies.set("Login Token", token, {
                     httpOnly: true,
                     secure: process.env.NODE_ENV === 'production',
                     path: '/',
                     // maxAge: 10,  // 10sec in seconds
+                    domain: 'https://health-medica.vercel.app',  // Ensure domain is correct
                     maxAge: 24 * 60 * 60,  // 1 day in seconds
+                    sameSite: 'Lax',  // Adjust to 'Strict' or 'None' if necessary
                 });
                 return response
-            } 
-            else{
+            }
+            else {
                 throw new Error("Incorrect Password")
                 // return NextResponse.json({result:"Incorrect Password", success: false})
             }
-            
+
         }
 
     } catch (error) {
